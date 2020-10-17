f3a4c857eddebe0af51928c7b28da39da2359a3b
Xu Jiangbo <xjbwd@163.com>
2020-10-17 16:14:18 +0800

    modify param for tianracer_pro

--- Src/param.c
+++ Src/param.c
@@ -21,12 +21,12 @@ const Param_t DefaultParam = {
   PI,//max_w
   4.5,//max_speed
   {0.2, 0.3, 0, 30},//{base_a, base_b, pwm_dead_zone, max_steer_angle}
-  {100, 10, 3, 500.0, 500.0},//p i d max_output i_limit
+  {50, 2, 1, 500.0, 500.0},//p i d max_output i_limit
   2400,//tick per lap
   65536,//max ticks
   10,//ctrl period
   10,//feedback period
-  1,//pose calc period
+  10,//pose calc period
   PARAM_TAIL
 };
 
